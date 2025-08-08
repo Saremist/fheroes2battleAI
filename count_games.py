651c7c92f82360b8cfc3687e25820297b23bf013
Kravig <milanw.2001@gmail.com>
2025-08-08 21:18:44 +0200

    Remove avg_reward from training log analysis
    Eliminated the 'avg_reward' field from the training log parsing in analyze_training_log.ipynb. Updated code cell execution counts and output images to reflect the change. Also updated the smoothed_avg_reward plot image and made minor changes in count_games.py.

--- count_games.py
+++ count_games.py
@@ -2,9 +2,15 @@ with open("training_log.txt", "r") as log_file:
     logs = log_file.readlines()
 
 games_count = 0
+total_time = 0
 
 for line in logs:
     number =int(line.split("|")[-1].split(" ")[-1] )
+    time = int(line.split("|")[1].split(" ")[-2].split(":")[-1])
     games_count += number
+    total_time += time
     
+total_hours = total_time // 3600
+
 print(games_count) 
+print(total_hours)
